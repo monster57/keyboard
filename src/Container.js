e3497940d3ed901785223b0abb884ccb9006e326
surajit57 <surajit@teampumpkin.com>
2016-04-17 19:39:05 +0530

    added basic render function to render a word

--- src/Container.js
+++ src/Container.js
@@ -2,16 +2,32 @@ import React from 'react';
 
 
 var data = [
-	[".",",","!",1],
-	["a","b","c",2],
-	["d","e","f",3],
-	["g","h","i",4],
-	["j","k","l",5],
-	["m","n","o",6],
-	["p","q","r","s",7],
-	["t","u","v",8],
-	["w","x","y","z",9],
-	["*"],
-	[0],
-	["#"]
-	 ];
+		[".",",","!",1],
+		["a","b","c",2],
+		["d","e","f",3],
+		["g","h","i",4],
+		["j","k","l",5],
+		["m","n","o",6],
+		["p","q","r","s",7],
+		["t","u","v",8],
+		["w","x","y","z",9],
+		["*"],
+		[0],
+		["#"]
+	];
+
+class Container extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Keypad</h1>
+			</div>
+		)
+	}
+}
+
+
+React.render(
+	<Container />,
+	document.getElementById('container')
+)
